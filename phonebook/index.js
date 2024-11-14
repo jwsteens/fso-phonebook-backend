126f3c0603fe18868c520b53a08eec0e5d0b00d7
Jeroen <jeroenwsteens@gmail.com>
2024-11-14 18:43:56 +0100

    Exercise 3.6

--- phonebook/index.js
+++ phonebook/index.js
@@ -4,6 +4,8 @@ const app = express()
 
 let persons = data
 
+const getRandomId = () => String(Math.floor(Math.random() * 999999999))
+
 app.use(express.json())
 
 app.get('/', ( request, response ) => {
@@ -33,6 +35,19 @@ app.get('/api/persons/:id', ( request, response ) => {
 app.delete('/api/persons/:id', ( request, response ) => {
   persons = persons.filter(p => p.id !== request.params.id)
   response.status(204).end()
+})
+
+app.post('/api/persons', ( request, response ) => {
+  const person = request.body
+  person.id = getRandomId()
+
+  if (!person.name) return response.status(400).json({ error: "no name was given" })
+  if (!person.number) return response.status(400).json({ error: "no number was given" })
+  if (persons.find(p => p.name === person.name)) return response.status(409).json({ error: "person with that name is already in phonebook" })
+  if (persons.find(p => p.number === person.number)) return response.status(409).json({ error: "person with that number is already in phonebook" })
+
+  persons.push(person)
+  response.json(person)
 })
 
 const PORT = 80
